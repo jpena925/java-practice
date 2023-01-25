39e17d5bb768c18062ef23e8e22bdf34d38acabf
Jack Pena <penajackc@gmail.com>
2023-01-25 16:17:09 -0700

    comparing multiple inputs

---
+++ part_2/ComparingNumbers.java
@@ -0,0 +1,22 @@
+package part_2;
+
+
+import java.util.Scanner;
+
+public class ComparingNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int first = Integer.valueOf(scanner.nextLine());
+        int sec = Integer.valueOf(scanner.nextLine());
+        
+        if(first < sec){
+            System.out.println(first + " is smaller than " + sec + ".");
+        } else if (first > sec){
+            System.out.println(first + " is greater than " + sec + ".");
+        } else {
+            System.out.println(first + " is equal to " + sec + ".");
+        }
+    }
+}
